cdcb8dc1eed766f747e81913774d266780604f67
waithimajr <malcolmwaithima@gmail.com>
2025-09-02 15:17:15 +0300

    perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,28 @@
+ 
+-File: performance.sql
+-Task: Optimize Complex Queries
+
+
+-Initial (unoptimized) query:
+-Retrieves bookings with user, property, and payment details.
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM Bookings b
+JOIN Users u ON b.user_id = u.user_id
+JOIN Properties p ON b.property_id = p.property_id
+LEFT JOIN Payments pay ON b.booking_id = pay.booking_id;
